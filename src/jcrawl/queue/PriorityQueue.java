2387fb5c376730f08cb3af7d57f967858ee318c5
Greg Gabelmann <greg.gabelmann@gmail.com>
2013-10-19 13:03:25 -0700

    Change PriorityQueue.java to use a TreeSet. This removes duplicate urls but still keeps the ordering.

--- src/jcrawl/queue/PriorityQueue.java
+++ src/jcrawl/queue/PriorityQueue.java
@@ -2,33 +2,36 @@ package jcrawl.queue;
 
 import java.util.Comparator;
 import java.util.Iterator;
+import java.util.NavigableSet;
+import java.util.TreeSet;
 
 /**
  * This Class sorts its urls according to the given Comparator.
+ * Duplicate urls are removed.
  */
 public class PriorityQueue implements Queue {
-	
-	private final java.util.PriorityQueue<String> queue;
-	
-	public PriorityQueue(final Comparator<String> comparator) {
-		this.queue = new java.util.PriorityQueue<String>(1024, comparator);
-	}
-	
-	@Override
-	public void add(final Iterator<String> urls) {
-		for (; urls.hasNext(); ) {
-			queue.add(urls.next());
-		}
-	}
-	
-	@Override
-	public String remove() {
-		return queue.poll();
-	}
-	
-	@Override
-	public int getSize() {
-		return queue.size();
-	}
-	
+   
+   private final NavigableSet<String> queue;
+   
+   public PriorityQueue(final Comparator<String> comparator) {
+      this.queue = new TreeSet<String>(comparator);
+   }
+   
+   @Override
+   public void add(final Iterator<String> urls) {
+      for (; urls.hasNext(); ) {
+         queue.add(urls.next());
+      }
+   }
+   
+   @Override
+   public String remove() {
+      return queue.pollFirst();
+   }
+   
+   @Override
+   public int getSize() {
+      return queue.size();
+   }
+   
 }
